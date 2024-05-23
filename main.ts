9221ed2c6be46a20b81ef04e67fadc78c668f28b
Suzanne <6035697@mborijnland.nl>
2024-05-23 13:01:52 +0200

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,13 @@
-
+let moveMotorZIP = Kitronik_Move_Motor.createMoveMotorZIPLED(4)
+let headlight1 = moveMotorZIP.range(0, 1)
+let headlight2 = moveMotorZIP.range(1, 2)
+let rearlight1 = moveMotorZIP.range(2, 3)
+let rearlight2 = moveMotorZIP.range(3, 4)
+basic.forever(function () {
+    Kitronik_Move_Motor.motorOn(Kitronik_Move_Motor.Motors.MotorLeft, Kitronik_Move_Motor.MotorDirection.Forward, 10)
+    basic.showIcon(IconNames.Happy)
+    headlight1.showColor(Kitronik_Move_Motor.colors(Kitronik_Move_Motor.ZipLedColors.Blue))
+    headlight2.showColor(Kitronik_Move_Motor.colors(Kitronik_Move_Motor.ZipLedColors.Purple))
+    rearlight1.showColor(Kitronik_Move_Motor.colors(Kitronik_Move_Motor.ZipLedColors.Blue))
+    rearlight2.showColor(Kitronik_Move_Motor.colors(Kitronik_Move_Motor.ZipLedColors.Purple))
+})
