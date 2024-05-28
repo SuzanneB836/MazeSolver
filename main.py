26ac1c230a40dece677ce1d77530c6138fd0a093
Suzanne <6035697@mborijnland.nl>
2024-05-28 17:57:45 +0200

    line following done yayy

--- main.py
+++ main.py
@@ -1,53 +1,33 @@
+sensorDifference = 0
+leftSensor = 0
+rightSensor = 0
+moveMotorZIP = Kitronik_Move_Motor.create_move_motor_zipled(4)
+headlight1 = moveMotorZIP.range(0, 1)
+headlight2 = moveMotorZIP.range(1, 2)
+rearlight1 = moveMotorZIP.range(2, 3)
+rearlight2 = moveMotorZIP.range(3, 4)
 
-
-# Function to read line sensor (assuming it returns values 0 or 1 for black or white)
-def read_line_sensor():
-    left_sensor = pin0.read_digital()  # Replace with your sensor pin
-    right_sensor = pin1.read_digital()  # Replace with your sensor pin
-    return left_sensor, right_sensor
-
-# Function to move forward
-def move_forward(speed=50):
-    motor_driver.motor_on(kitronik_motor_driver.MOTOR_LEFT, kitronik_motor_driver.MOTORDIRECTION_FORWARD, speed)
-    motor_driver.motor_on(kitronik_motor_driver.MOTOR_RIGHT, kitronik_motor_driver.MOTORDIRECTION_FORWARD, speed)
-
-# Function to stop
-def stop():
-    motor_driver.motor_off(kitronik_motor_driver.MOTOR_LEFT)
-    motor_driver.motor_off(kitronik_motor_driver.MOTOR_RIGHT)
-
-# Function to turn left
-def turn_left():
-    motor_driver.motor_on(kitronik_motor_driver.MOTOR_LEFT, kitronik_motor_driver.MOTORDIRECTION_REVERSE, 50)
-    motor_driver.motor_on(kitronik_motor_driver.MOTOR_RIGHT, kitronik_motor_driver.MOTORDIRECTION_FORWARD, 50)
-    sleep(300)  # Adjust timing as necessary
-    stop()
-
-# Function to turn right
-def turn_right():
-    motor_driver.motor_on(kitronik_motor_driver.MOTOR_LEFT, kitronik_motor_driver.MOTORDIRECTION_FORWARD, 50)
-    motor_driver.motor_on(kitronik_motor_driver.MOTOR_RIGHT, kitronik_motor_driver.MOTORDIRECTION_REVERSE, 50)
-    sleep(300)  # Adjust timing as necessary
-    stop()
-
-# Main loop to solve the maze
-while True:
-    left_sensor, right_sensor = read_line_sensor()
-
-    if left_sensor == 1 and right_sensor == 1:
-        # Both sensors on the line, move forward
-        move_forward()
-    elif left_sensor == 1 and right_sensor == 0:
-        # Left sensor on the line, turn left
-        turn_left()
-    elif left_sensor == 0 and right_sensor == 1:
-        # Right sensor on the line, turn right
-        turn_right()
+def on_forever():
+    global rightSensor, leftSensor, sensorDifference
+    basic.show_icon(IconNames.HAPPY)
+    headlight1.show_color(Kitronik_Move_Motor.colors(Kitronik_Move_Motor.ZipLedColors.BLUE))
+    headlight2.show_color(Kitronik_Move_Motor.colors(Kitronik_Move_Motor.ZipLedColors.PURPLE))
+    rearlight1.show_color(Kitronik_Move_Motor.colors(Kitronik_Move_Motor.ZipLedColors.BLUE))
+    rearlight2.show_color(Kitronik_Move_Motor.colors(Kitronik_Move_Motor.ZipLedColors.PURPLE))
+    rightSensor = Kitronik_Move_Motor.read_sensor(Kitronik_Move_Motor.LfSensor.RIGHT)
+    leftSensor = Kitronik_Move_Motor.read_sensor(Kitronik_Move_Motor.LfSensor.LEFT)
+    sensorDifference = abs(leftSensor - rightSensor)
+    if sensorDifference > 10:
+        if leftSensor > rightSensor:
+            Kitronik_Move_Motor.motor_off(Kitronik_Move_Motor.Motors.MOTOR_RIGHT)
+            Kitronik_Move_Motor.motor_on(Kitronik_Move_Motor.Motors.MOTOR_LEFT,
+                Kitronik_Move_Motor.MotorDirection.FORWARD,
+                30)
+        else:
+            Kitronik_Move_Motor.motor_off(Kitronik_Move_Motor.Motors.MOTOR_LEFT)
+            Kitronik_Move_Motor.motor_on(Kitronik_Move_Motor.Motors.MOTOR_RIGHT,
+                Kitronik_Move_Motor.MotorDirection.FORWARD,
+                30)
     else:
-        # Both sensors off the line, stop and decide next move
-        stop()
-        sleep(100)
-        # Basic strategy for maze solving could be added here
-        # This example stops the robot if both sensors are off the line
-
-    sleep(100)  # Short delay to prevent overwhelming the sensors
+        Kitronik_Move_Motor.move(Kitronik_Move_Motor.DriveDirections.FORWARD, 30)
+basic.forever(on_forever)
--- main.ts
+++ main.ts
@@ -1,3 +1,6 @@
+let sensorDifference = 0
+let leftSensor = 0
+let rightSensor = 0
 let moveMotorZIP = Kitronik_Move_Motor.createMoveMotorZIPLED(4)
 let headlight1 = moveMotorZIP.range(0, 1)
 let headlight2 = moveMotorZIP.range(1, 2)
@@ -5,9 +8,22 @@ let rearlight1 = moveMotorZIP.range(2, 3)
 let rearlight2 = moveMotorZIP.range(3, 4)
 basic.forever(function () {
     basic.showIcon(IconNames.Happy)
-    Kitronik_Move_Motor.motorOn(Kitronik_Move_Motor.Motors.MotorLeft, Kitronik_Move_Motor.MotorDirection.Forward, 10)
     headlight1.showColor(Kitronik_Move_Motor.colors(Kitronik_Move_Motor.ZipLedColors.Blue))
     headlight2.showColor(Kitronik_Move_Motor.colors(Kitronik_Move_Motor.ZipLedColors.Purple))
     rearlight1.showColor(Kitronik_Move_Motor.colors(Kitronik_Move_Motor.ZipLedColors.Blue))
     rearlight2.showColor(Kitronik_Move_Motor.colors(Kitronik_Move_Motor.ZipLedColors.Purple))
+    rightSensor = Kitronik_Move_Motor.readSensor(Kitronik_Move_Motor.LfSensor.Right)
+    leftSensor = Kitronik_Move_Motor.readSensor(Kitronik_Move_Motor.LfSensor.Left)
+    sensorDifference = Math.abs(leftSensor - rightSensor)
+    if (sensorDifference > 10) {
+        if (leftSensor > rightSensor) {
+            Kitronik_Move_Motor.motorOff(Kitronik_Move_Motor.Motors.MotorRight)
+            Kitronik_Move_Motor.motorOn(Kitronik_Move_Motor.Motors.MotorLeft, Kitronik_Move_Motor.MotorDirection.Forward, 30)
+        } else {
+            Kitronik_Move_Motor.motorOff(Kitronik_Move_Motor.Motors.MotorLeft)
+            Kitronik_Move_Motor.motorOn(Kitronik_Move_Motor.Motors.MotorRight, Kitronik_Move_Motor.MotorDirection.Forward, 30)
+        }
+    } else {
+        Kitronik_Move_Motor.move(Kitronik_Move_Motor.DriveDirections.Forward, 30)
+    }
 })
