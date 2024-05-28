d9d13d1864db779a8b677ae86a8497c09458c7c1
Suzanne <6035697@mborijnland.nl>
2024-05-28 18:00:53 +0200

    Update main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -1,22 +1,18 @@
-sensorDifference = 0
-leftSensor = 0
-rightSensor = 0
 moveMotorZIP = Kitronik_Move_Motor.create_move_motor_zipled(4)
 headlight1 = moveMotorZIP.range(0, 1)
 headlight2 = moveMotorZIP.range(1, 2)
 rearlight1 = moveMotorZIP.range(2, 3)
 rearlight2 = moveMotorZIP.range(3, 4)
+rightSensor = Kitronik_Move_Motor.read_sensor(Kitronik_Move_Motor.LfSensor.RIGHT)
+leftSensor = Kitronik_Move_Motor.read_sensor(Kitronik_Move_Motor.LfSensor.LEFT)
+sensorDifference = abs(leftSensor - rightSensor)
 
 def on_forever():
-    global rightSensor, leftSensor, sensorDifference
     basic.show_icon(IconNames.HAPPY)
     headlight1.show_color(Kitronik_Move_Motor.colors(Kitronik_Move_Motor.ZipLedColors.BLUE))
     headlight2.show_color(Kitronik_Move_Motor.colors(Kitronik_Move_Motor.ZipLedColors.PURPLE))
     rearlight1.show_color(Kitronik_Move_Motor.colors(Kitronik_Move_Motor.ZipLedColors.BLUE))
     rearlight2.show_color(Kitronik_Move_Motor.colors(Kitronik_Move_Motor.ZipLedColors.PURPLE))
-    rightSensor = Kitronik_Move_Motor.read_sensor(Kitronik_Move_Motor.LfSensor.RIGHT)
-    leftSensor = Kitronik_Move_Motor.read_sensor(Kitronik_Move_Motor.LfSensor.LEFT)
-    sensorDifference = abs(leftSensor - rightSensor)
     if sensorDifference > 10:
         if leftSensor > rightSensor:
             Kitronik_Move_Motor.motor_off(Kitronik_Move_Motor.Motors.MOTOR_RIGHT)
--- main.ts
+++ main.ts
@@ -1,20 +1,17 @@
-let sensorDifference = 0
-let leftSensor = 0
-let rightSensor = 0
 let moveMotorZIP = Kitronik_Move_Motor.createMoveMotorZIPLED(4)
 let headlight1 = moveMotorZIP.range(0, 1)
 let headlight2 = moveMotorZIP.range(1, 2)
 let rearlight1 = moveMotorZIP.range(2, 3)
 let rearlight2 = moveMotorZIP.range(3, 4)
+let rightSensor = Kitronik_Move_Motor.readSensor(Kitronik_Move_Motor.LfSensor.Right)
+let leftSensor = Kitronik_Move_Motor.readSensor(Kitronik_Move_Motor.LfSensor.Left)
+let sensorDifference = Math.abs(leftSensor - rightSensor)
 basic.forever(function () {
     basic.showIcon(IconNames.Happy)
     headlight1.showColor(Kitronik_Move_Motor.colors(Kitronik_Move_Motor.ZipLedColors.Blue))
     headlight2.showColor(Kitronik_Move_Motor.colors(Kitronik_Move_Motor.ZipLedColors.Purple))
     rearlight1.showColor(Kitronik_Move_Motor.colors(Kitronik_Move_Motor.ZipLedColors.Blue))
     rearlight2.showColor(Kitronik_Move_Motor.colors(Kitronik_Move_Motor.ZipLedColors.Purple))
-    rightSensor = Kitronik_Move_Motor.readSensor(Kitronik_Move_Motor.LfSensor.Right)
-    leftSensor = Kitronik_Move_Motor.readSensor(Kitronik_Move_Motor.LfSensor.Left)
-    sensorDifference = Math.abs(leftSensor - rightSensor)
     if (sensorDifference > 10) {
         if (leftSensor > rightSensor) {
             Kitronik_Move_Motor.motorOff(Kitronik_Move_Motor.Motors.MotorRight)
