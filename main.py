0ba9bd7a18d9258c8d7bddefdf6d10a7b14d4d3b
Suzanne <6035697@mborijnland.nl>
2024-05-30 13:18:52 +0200

    Update main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -1,29 +1,50 @@
-moveMotorZIP = Kitronik_Move_Motor.create_move_motor_zipled(4)
-headlight1 = moveMotorZIP.range(0, 1)
-headlight2 = moveMotorZIP.range(1, 2)
-rearlight1 = moveMotorZIP.range(2, 3)
-rearlight2 = moveMotorZIP.range(3, 4)
-rightSensor = Kitronik_Move_Motor.read_sensor(Kitronik_Move_Motor.LfSensor.RIGHT)
-leftSensor = Kitronik_Move_Motor.read_sensor(Kitronik_Move_Motor.LfSensor.LEFT)
-sensorDifference = abs(leftSensor - rightSensor)
+leftSensor = 0
+rightSensor = 0
+sensorDifference = 0
+
+def on_button_pressed_a():
+    pass
+input.on_button_pressed(Button.A, on_button_pressed_a)
+
+def on_button_pressed_b():
+    Kitronik_Move_Motor.stop()
+input.on_button_pressed(Button.B, on_button_pressed_b)
+
+def on_every_interval():
+    basic.show_leds("""
+        # . . . .
+        # . . . .
+        # . . . .
+        # . . . .
+        # # # # #
+        """)
+    basic.show_string("" + str((leftSensor)))
+    basic.show_leds("""
+        # # # # #
+        # . . . #
+        # # # # #
+        # # . . .
+        # . # . .
+        """)
+    basic.show_string("" + str((rightSensor)))
+loops.every_interval(500, on_every_interval)
 
 def on_forever():
-    basic.show_icon(IconNames.HAPPY)
-    headlight1.show_color(Kitronik_Move_Motor.colors(Kitronik_Move_Motor.ZipLedColors.BLUE))
-    headlight2.show_color(Kitronik_Move_Motor.colors(Kitronik_Move_Motor.ZipLedColors.PURPLE))
-    rearlight1.show_color(Kitronik_Move_Motor.colors(Kitronik_Move_Motor.ZipLedColors.BLUE))
-    rearlight2.show_color(Kitronik_Move_Motor.colors(Kitronik_Move_Motor.ZipLedColors.PURPLE))
-    if sensorDifference > 10:
+    global rightSensor, leftSensor, sensorDifference
+    rightSensor = Kitronik_Move_Motor.read_sensor(Kitronik_Move_Motor.LfSensor.RIGHT)
+    leftSensor = Kitronik_Move_Motor.read_sensor(Kitronik_Move_Motor.LfSensor.LEFT)
+    sensorDifference = abs(leftSensor - rightSensor)
+    if leftSensor < 10 and rightSensor < 10:
+        Kitronik_Move_Motor.move(Kitronik_Move_Motor.DriveDirections.FORWARD, 30)
+    else:
         if leftSensor > rightSensor:
-            Kitronik_Move_Motor.motor_off(Kitronik_Move_Motor.Motors.MOTOR_RIGHT)
-            Kitronik_Move_Motor.motor_on(Kitronik_Move_Motor.Motors.MOTOR_LEFT,
-                Kitronik_Move_Motor.MotorDirection.FORWARD,
-                30)
-        else:
             Kitronik_Move_Motor.motor_off(Kitronik_Move_Motor.Motors.MOTOR_LEFT)
             Kitronik_Move_Motor.motor_on(Kitronik_Move_Motor.Motors.MOTOR_RIGHT,
                 Kitronik_Move_Motor.MotorDirection.FORWARD,
                 30)
-    else:
-        Kitronik_Move_Motor.move(Kitronik_Move_Motor.DriveDirections.FORWARD, 30)
+        else:
+            Kitronik_Move_Motor.motor_off(Kitronik_Move_Motor.Motors.MOTOR_RIGHT)
+            Kitronik_Move_Motor.motor_on(Kitronik_Move_Motor.Motors.MOTOR_LEFT,
+                Kitronik_Move_Motor.MotorDirection.FORWARD,
+                30)
 basic.forever(on_forever)
--- main.ts
+++ main.ts
@@ -1,26 +1,43 @@
-let moveMotorZIP = Kitronik_Move_Motor.createMoveMotorZIPLED(4)
-let headlight1 = moveMotorZIP.range(0, 1)
-let headlight2 = moveMotorZIP.range(1, 2)
-let rearlight1 = moveMotorZIP.range(2, 3)
-let rearlight2 = moveMotorZIP.range(3, 4)
-let rightSensor = Kitronik_Move_Motor.readSensor(Kitronik_Move_Motor.LfSensor.Right)
-let leftSensor = Kitronik_Move_Motor.readSensor(Kitronik_Move_Motor.LfSensor.Left)
-let sensorDifference = Math.abs(leftSensor - rightSensor)
+let leftSensor = 0
+let rightSensor = 0
+let sensorDifference = 0
+input.onButtonPressed(Button.A, function () {
+	
+})
+input.onButtonPressed(Button.B, function () {
+    Kitronik_Move_Motor.stop()
+})
+loops.everyInterval(500, function () {
+    basic.showLeds(`
+        # . . . .
+        # . . . .
+        # . . . .
+        # . . . .
+        # # # # #
+        `)
+    basic.showString("" + (leftSensor))
+    basic.showLeds(`
+        # # # # #
+        # . . . #
+        # # # # #
+        # # . . .
+        # . # . .
+        `)
+    basic.showString("" + (rightSensor))
+})
 basic.forever(function () {
-    basic.showIcon(IconNames.Happy)
-    headlight1.showColor(Kitronik_Move_Motor.colors(Kitronik_Move_Motor.ZipLedColors.Blue))
-    headlight2.showColor(Kitronik_Move_Motor.colors(Kitronik_Move_Motor.ZipLedColors.Purple))
-    rearlight1.showColor(Kitronik_Move_Motor.colors(Kitronik_Move_Motor.ZipLedColors.Blue))
-    rearlight2.showColor(Kitronik_Move_Motor.colors(Kitronik_Move_Motor.ZipLedColors.Purple))
-    if (sensorDifference > 10) {
+    rightSensor = Kitronik_Move_Motor.readSensor(Kitronik_Move_Motor.LfSensor.Right)
+    leftSensor = Kitronik_Move_Motor.readSensor(Kitronik_Move_Motor.LfSensor.Left)
+    sensorDifference = Math.abs(leftSensor - rightSensor)
+    if (leftSensor < 10 && rightSensor < 10) {
+        Kitronik_Move_Motor.move(Kitronik_Move_Motor.DriveDirections.Forward, 20)
+    } else {
         if (leftSensor > rightSensor) {
             Kitronik_Move_Motor.motorOff(Kitronik_Move_Motor.Motors.MotorRight)
-            Kitronik_Move_Motor.motorOn(Kitronik_Move_Motor.Motors.MotorLeft, Kitronik_Move_Motor.MotorDirection.Forward, 30)
+            Kitronik_Move_Motor.motorOn(Kitronik_Move_Motor.Motors.MotorLeft, Kitronik_Move_Motor.MotorDirection.Forward, 20)
         } else {
             Kitronik_Move_Motor.motorOff(Kitronik_Move_Motor.Motors.MotorLeft)
-            Kitronik_Move_Motor.motorOn(Kitronik_Move_Motor.Motors.MotorRight, Kitronik_Move_Motor.MotorDirection.Forward, 30)
+            Kitronik_Move_Motor.motorOn(Kitronik_Move_Motor.Motors.MotorRight, Kitronik_Move_Motor.MotorDirection.Forward, 20)
         }
-    } else {
-        Kitronik_Move_Motor.move(Kitronik_Move_Motor.DriveDirections.Forward, 30)
     }
 })
