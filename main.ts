cbc8b302ce078a2f482538cf1b6873e542c57e37
Suzanne <6035697@mborijnland.nl>
2024-06-04 14:38:11 +0200

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -5,11 +5,10 @@ let headlight1 = moveMotorZIP.range(0, 1)
 let headlight2 = moveMotorZIP.range(1, 2)
 let rearlight1 = moveMotorZIP.range(2, 3)
 let rearlight2 = moveMotorZIP.range(3, 4)
-loops.everyInterval(1000, function on_every_interval() {
-    basic.showString("59")
+loops.everyInterval(1000, function () {
+    basic.showIcon(IconNames.Heart)
 })
-basic.forever(function on_forever() {
-    
+basic.forever(function () {
     headlight1.showColor(Kitronik_Move_Motor.colors(Kitronik_Move_Motor.ZipLedColors.Red))
     headlight2.showColor(Kitronik_Move_Motor.colors(Kitronik_Move_Motor.ZipLedColors.White))
     rearlight1.showColor(Kitronik_Move_Motor.colors(Kitronik_Move_Motor.ZipLedColors.Red))
@@ -17,16 +16,17 @@ basic.forever(function on_forever() {
     rightSensor = Kitronik_Move_Motor.readSensor(Kitronik_Move_Motor.LfSensor.Right)
     leftSensor = Kitronik_Move_Motor.readSensor(Kitronik_Move_Motor.LfSensor.Left)
     if (leftSensor < 10 && rightSensor < 10) {
-        Kitronik_Move_Motor.move(Kitronik_Move_Motor.DriveDirections.Forward, 22)
+        Kitronik_Move_Motor.move(Kitronik_Move_Motor.DriveDirections.Forward, 20)
     } else if (leftSensor > 90 && rightSensor > 90) {
         Kitronik_Move_Motor.motorOn(Kitronik_Move_Motor.Motors.MotorLeft, Kitronik_Move_Motor.MotorDirection.Forward, 20)
         Kitronik_Move_Motor.motorOn(Kitronik_Move_Motor.Motors.MotorRight, Kitronik_Move_Motor.MotorDirection.Reverse, 20)
-    } else if (leftSensor > rightSensor) {
-        Kitronik_Move_Motor.motorOff(Kitronik_Move_Motor.Motors.MotorRight)
-        Kitronik_Move_Motor.motorOn(Kitronik_Move_Motor.Motors.MotorLeft, Kitronik_Move_Motor.MotorDirection.Forward, 15)
     } else {
-        Kitronik_Move_Motor.motorOff(Kitronik_Move_Motor.Motors.MotorLeft)
-        Kitronik_Move_Motor.motorOn(Kitronik_Move_Motor.Motors.MotorRight, Kitronik_Move_Motor.MotorDirection.Forward, 15)
+        if (leftSensor > rightSensor) {
+            Kitronik_Move_Motor.motorOff(Kitronik_Move_Motor.Motors.MotorRight)
+            Kitronik_Move_Motor.motorOn(Kitronik_Move_Motor.Motors.MotorLeft, Kitronik_Move_Motor.MotorDirection.Forward, 15)
+        } else {
+            Kitronik_Move_Motor.motorOff(Kitronik_Move_Motor.Motors.MotorLeft)
+            Kitronik_Move_Motor.motorOn(Kitronik_Move_Motor.Motors.MotorRight, Kitronik_Move_Motor.MotorDirection.Forward, 15)
+        }
     }
-    
 })
